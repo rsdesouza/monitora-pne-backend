df2cf12e21b9dcebc8d70ea19288029a243ad137
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-11-26 21:45:51 -0300

    feat: add configs do cors

---
+++ src/main/java/school/cesar/monitora_pne_backend/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package school.cesar.monitora_pne_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Permite acesso do frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
