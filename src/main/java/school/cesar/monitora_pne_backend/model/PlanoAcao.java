42212c4b2d772779c7863e086a7f89944a6fe88d
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-11-27 20:41:14 -0300

    feat: updade na entity plano de ação

--- src/main/java/school/cesar/monitora_pne_backend/model/PlanoAcao.java
+++ src/main/java/school/cesar/monitora_pne_backend/model/PlanoAcao.java
@@ -1,8 +1,8 @@
 package school.cesar.monitora_pne_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Data;
-
-import java.time.LocalDate;
 
 @Data
 public class PlanoAcao {
@@ -11,7 +11,21 @@ public class PlanoAcao {
     private String estrategia;
     private String planoDeAcao;
     private String observacao;
-    private String status;
+    private Status status;
     private String dataInicio;
     private String dataFim;
+
+    public enum Status {
+        CONCLUIDO, EM_ANDAMENTO, ATRASADO, DESCONTINUADO;
+
+        @JsonCreator
+        public static Status fromValue(String value) {
+            return Status.valueOf(value.toUpperCase().replace(" ", "_"));
+        }
+
+        @JsonValue
+        public String toValue() {
+            return this.name().toLowerCase().replace("_", " ");
+        }
+    }
 }
