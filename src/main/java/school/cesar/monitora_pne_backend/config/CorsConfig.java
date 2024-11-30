f667e9feb9b6795e9607349675d66af24d6db2e5
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-11-29 21:50:39 -0300

    feat: ajustes nos CORS da app

--- src/main/java/school/cesar/monitora_pne_backend/config/CorsConfig.java
+++ src/main/java/school/cesar/monitora_pne_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Permite acesso do frontend
+                        .allowedOrigins("https://monitora-pne-frontend.web.app") // Permite acesso do frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
